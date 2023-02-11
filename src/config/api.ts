13b12f1e3a4a2ca63ca9a355db9cf807a0e9ea87
TsitoUw <tsitohainanirinarak2@gmail.com>
2023-02-12 00:34:21 +0300

    changing config

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,3 +1,3 @@
-export const baseUrl = "http://localhost:8000/api";
+export const baseUrl = "https://musicc-back.vercel.app/api";
 
-export const publicUrl = "http://localhost:8000/";
+export const publicUrl = "https://musicc-back.vercel.app/";
