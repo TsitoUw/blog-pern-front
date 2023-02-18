58a21843d5395506042bd4d250c5b50890ec9762
TsitoUw <tsitohainanirinarak2@gmail.com>
2023-02-18 06:49:27 +0300

    upload still loading data to ram

--- src/components/parts/AudioPlayer.tsx
+++ src/components/parts/AudioPlayer.tsx
@@ -6,12 +6,16 @@ import PauseIcon from "@mui/icons-material/Pause";
 import VolumeUpIcon from "@mui/icons-material/VolumeUp";
 import VolumeOffIcon from "@mui/icons-material/VolumeOff";
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 401ef41 (upload still loading data to ram)
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import SkipNextIcon from "@mui/icons-material/SkipNext";
 import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
 import ShuffleIcon from "@mui/icons-material/Shuffle";
 import RepeatOneIcon from "@mui/icons-material/RepeatOne";
+<<<<<<< HEAD
 
 import "./AudioPlayer.css";
 import { SongAttributes } from "../../types/Audio";
@@ -25,6 +29,12 @@ import RepeatOneIcon from '@mui/icons-material/RepeatOne';
 
 import "./AudioPlayer.css";
 >>>>>>> a715526 (audio player layout/ basic functionality)
+=======
+
+import "./AudioPlayer.css";
+import { SongAttributes } from "../../types/Audio";
+
+>>>>>>> 401ef41 (upload still loading data to ram)
 type Props = {
   className: string;
 };
@@ -34,11 +44,17 @@ const AudioPlayer = ({ className }: Props) => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [isMute, setIsMute] = useState(false);
 <<<<<<< HEAD
+<<<<<<< HEAD
   const [isLooping, setIsLooping] = useState(false);
   const [isShuffling, setIsShuffling] = useState(false);
   const [isFavorite, setIsFavorite] = useState(false);
 =======
 >>>>>>> a715526 (audio player layout/ basic functionality)
+=======
+  const [isLooping, setIsLooping] = useState(false);
+  const [isShuffling, setIsShuffling] = useState(false);
+  const [isFavorite, setIsFavorite] = useState(false);
+>>>>>>> 401ef41 (upload still loading data to ram)
   const [seek, setSeek] = useState(0);
   const [seekMax, setSeekMax] = useState(100);
   const [volume, setVolume] = useState(100);
@@ -47,6 +63,9 @@ const AudioPlayer = ({ className }: Props) => {
 
   const audioRef = useRef<HTMLAudioElement>(null);
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 401ef41 (upload still loading data to ram)
   const containerRef = useRef<HTMLDivElement>(null);
 
   const [index, setIndex] = useState(0);
@@ -68,6 +87,7 @@ const AudioPlayer = ({ className }: Props) => {
       url: publicUrl + "someSong/music.mp3",
     },
   ];
+<<<<<<< HEAD
 
   function playPause() {
     audioRef.current && !isPlaying ? audioRef.current?.play() : audioRef.current?.pause();
@@ -76,6 +96,11 @@ const AudioPlayer = ({ className }: Props) => {
   function playPause() {
     !isPlaying ? audioRef.current?.play() : audioRef.current?.pause();
 >>>>>>> a715526 (audio player layout/ basic functionality)
+=======
+
+  function playPause() {
+    audioRef.current && !isPlaying ? audioRef.current?.play() : audioRef.current?.pause();
+>>>>>>> 401ef41 (upload still loading data to ram)
     setIsPlaying((prev) => !prev);
   }
 
@@ -84,6 +109,9 @@ const AudioPlayer = ({ className }: Props) => {
   }
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 401ef41 (upload still loading data to ram)
   function loopUnloop() {
     setIsLooping((prev) => !prev);
   }
@@ -123,8 +151,11 @@ const AudioPlayer = ({ className }: Props) => {
     }
   }
   /** */
+<<<<<<< HEAD
 =======
 >>>>>>> a715526 (audio player layout/ basic functionality)
+=======
+>>>>>>> 401ef41 (upload still loading data to ram)
   function calclulateTime(secs: number) {
     const minutes = Math.floor(secs / 60);
     const seconds = Math.floor(secs % 60);
@@ -145,13 +176,19 @@ const AudioPlayer = ({ className }: Props) => {
       setSeek(audioRef.current.currentTime);
     }
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 401ef41 (upload still loading data to ram)
     if (containerRef.current) {
       containerRef.current.style.setProperty("--seek-before-width", (seek / seekMax) * 100 + "%");
     }
 
     if(audioRef.current?.ended && !isLooping) nextSong();
+<<<<<<< HEAD
 =======
 >>>>>>> a715526 (audio player layout/ basic functionality)
+=======
+>>>>>>> 401ef41 (upload still loading data to ram)
   }
 
   function audioSeek(value: number) {
@@ -161,10 +198,15 @@ const AudioPlayer = ({ className }: Props) => {
 
   function changeVolume(value: number) {
 <<<<<<< HEAD
+<<<<<<< HEAD
     if (containerRef.current)
       containerRef.current.style.setProperty("--volume-before-width", (value / 100) * 100 + "%");
 =======
 >>>>>>> a715526 (audio player layout/ basic functionality)
+=======
+    if (containerRef.current)
+      containerRef.current.style.setProperty("--volume-before-width", (value / 100) * 100 + "%");
+>>>>>>> 401ef41 (upload still loading data to ram)
     if (audioRef.current) audioRef.current.volume = value / 100;
     setVolume(value);
   }
@@ -175,13 +217,19 @@ const AudioPlayer = ({ className }: Props) => {
   }, [volume]);
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 401ef41 (upload still loading data to ram)
   useEffect(()=>{
     if (containerRef.current)
       containerRef.current.style.setProperty("--volume-before-width", (volume / 100) * 100 + "%");
   },[])
 
+<<<<<<< HEAD
 =======
 >>>>>>> a715526 (audio player layout/ basic functionality)
+=======
+>>>>>>> 401ef41 (upload still loading data to ram)
   if ("mediaSession" in navigator) {
     navigator.mediaSession.metadata = new MediaMetadata({
       artist: song?.currentSong?.artist,
@@ -220,6 +268,9 @@ const AudioPlayer = ({ className }: Props) => {
       ],
     });
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 401ef41 (upload still loading data to ram)
     navigator.mediaSession.setActionHandler("pause", () => {
       audioRef.current && audioRef.current?.pause();
       setIsPlaying(false);
@@ -228,6 +279,7 @@ const AudioPlayer = ({ className }: Props) => {
       audioRef.current && audioRef.current?.play();
       setIsPlaying(true);
     });
+<<<<<<< HEAD
   }
 
   return (
@@ -272,35 +324,56 @@ const AudioPlayer = ({ className }: Props) => {
           </div>
           <input
 =======
+=======
+>>>>>>> 401ef41 (upload still loading data to ram)
   }
 
   return (
-    <div className={`player-container | z-0 flex w-full bg-slate-900 p-4 md:p-1 ${className}`}>
-      <audio
-        className="hidden"
-        preload="metadata"
-        controls
-        src={song?.currentSong?.url}
-        ref={audioRef}
-        onLoadedMetadata={displayInfo}
-        onTimeUpdate={audioProgress}
-        onPlay={() => setIsPlaying(true)}
-        onPause={() => setIsPlaying(false)}
-        muted={isMute}
-      />
-      <div className="action flex justify-evenly items-center w-1/12 md:w-2/12">
-        <button className="hidden md:flex" onClick={playPause}><SkipPreviousIcon/></button>
-        <button className="flex text-3xl" onClick={playPause}>{isPlaying ? <PauseIcon fontSize="inherit"/> : <PlayArrowIcon fontSize="inherit"/>}</button>
-        <button className="hidden md:flex" onClick={playPause}><SkipNextIcon/></button>
-        <button className="hidden lg:flex" onClick={playPause}><ShuffleIcon/></button>
-        <button className="hidden lg:flex"onClick={playPause}><RepeatOneIcon/></button>
-      </div>
-      <div className="duration flex w-10/12 md:w-8/12">
-        <div id="current-time" className="w-2/12 lg:w-1/12 flex items-center justify-center text-sm">
-          {currentTime}
+    <div
+      className={`player-container | z-0 flex w-full justify-center bg-slate-900 backdrop-blur-md bg-opacity-80 p-2 md:p-1 ${className}`}
+      ref={containerRef}
+    >
+      <div className="flex w-full md:w-11/12">
+        <audio
+          className="hidden"
+          preload="metadata"
+          controls
+          src={song?.currentSong?.url}
+          ref={audioRef}
+          onLoadedMetadata={displayInfo}
+          onTimeUpdate={audioProgress}
+          onPlay={() => setIsPlaying(true)}
+          onPause={() => setIsPlaying(false)}
+          muted={isMute}
+          loop={isLooping}
+        />
+        <div className="action flex justify-evenly items-center w-1/12 md:w-2/12">
+          <button className="hidden md:flex text-neutral-300" onClick={prevSong}>
+            <SkipPreviousIcon />
+          </button>
+          <button className="flex text-3xl text-neutral-300" onClick={playPause}>
+            {isPlaying ? <PauseIcon fontSize="inherit" /> : <PlayArrowIcon fontSize="inherit" />}
+          </button>
+          <button className="hidden md:flex" onClick={nextSong}>
+            <SkipNextIcon />
+          </button>
+          <button className="hidden lg:flex" onClick={shuffleUnshuffle}>
+            <ShuffleIcon className={isShuffling ? "text-rose-600" : "text-neutral-300"} />
+          </button>
+          <button className="hidden lg:flex" onClick={loopUnloop}>
+            <RepeatOneIcon className={isLooping ? "text-rose-600" : "text-neutral-300"} />
+          </button>
         </div>
+<<<<<<< HEAD
         <input
 >>>>>>> a715526 (audio player layout/ basic functionality)
+=======
+        <div className="duration hidden md:flex w-10/12 md:w-6/12 lg:w-9/12">
+          <div id="current-time" className="w-2/12 lg:w-1/12 flex items-center justify-center text-sm">
+            {currentTime}
+          </div>
+          <input
+>>>>>>> 401ef41 (upload still loading data to ram)
             type="range"
             name="seek-slider"
             id="seek-slider"
@@ -312,10 +385,14 @@ const AudioPlayer = ({ className }: Props) => {
             max={seekMax}
           />
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 401ef41 (upload still loading data to ram)
 
           <div id="duration-time" className="w-2/12 lg:w-1/12 flex items-center justify-center text-sm">
             {durationTime}
           </div>
+<<<<<<< HEAD
         </div>
         <div className="volume-container hidden lg:flex w-10 relative  items-center justify-center">
           <div className="volume flex relative items-center justify-center">
@@ -363,28 +440,52 @@ const AudioPlayer = ({ className }: Props) => {
           
         <div id="duration-time" className="w-2/12 lg:w-1/12 flex items-center justify-center text-sm">
           {durationTime}
+=======
+>>>>>>> 401ef41 (upload still loading data to ram)
         </div>
-      </div>
-      <div className="volume-container hidden lg:flex w-1/12 relative  items-center justify-center">
-        <div className="volume flex relative items-center justify-center">
-          <div className="input-container">
-            <input
-              aria-orientation="vertical"
-              type="range"
-              name="volume-slider"
-              className="volume-slider"
-              value={volume}
-              onInput={(e) => {
-                changeVolume(Number(e.currentTarget.value));
-              }}
-              max="100"
+        <div className="volume-container hidden lg:flex w-10 relative  items-center justify-center">
+          <div className="volume flex relative items-center justify-center">
+            <div className="input-container ">
+              <input
+                type="range"
+                name="volume-slider"
+                className="volume-slider"
+                value={volume}
+                onInput={(e) => {
+                  changeVolume(Number(e.currentTarget.value));
+                }}
+                max="100"
+              />
+            </div>
+            <button className="volume-btn text-neutral-300 flex items-center justify-center" onClick={muteUnmute}>
+              {!isMute ? <VolumeUpIcon fontSize="small"/> : <VolumeOffIcon fontSize="small"/>}
+            </button>
+          </div>
+        </div>
+
+        <div className="info w-full md:w-4/12 lg:w-3/12 flex">
+          <div className="artwork hidden w-10 lg:flex items-center justify-center">
+            <img
+              src={publicUrl + "artworks/sary.png"}
+              className="w-full aspect-square object-cover rounded-sm"
             />
           </div>
-          <button className="volume-btn" onClick={muteUnmute}>
-            {!isMute ? <VolumeUpIcon /> : <VolumeOffIcon />}
+          <div className="about  md:flex w-4/5 lg:w-8/12 flex-col p-1 px-3 justify-evenly">
+            <div className="artist text-sm overflow-hidden whitespace-nowrap truncate text-neutral-300">
+              {song?.currentSong?.artist}
+            </div>
+            <div className="title overflow-hidden whitespace-nowrap truncate">{song?.currentSong?.title}</div>
+          </div>
+          <button className="action w-1/5 md:w-max flex items-center justify-center" onClick={favoriteUnfavorite}>
+            {isFavorite ? (
+              <FavoriteIcon className="mx-2 text-rose-600" />
+            ) : (
+              <FavoriteBorderIcon className="mx-2 text-neutral-300" />
+            )}
           </button>
         </div>
       </div>
+<<<<<<< HEAD
 
       <div className="info w-1/12 md:w-4/12 flex">
         <div className="artwork hidden lg:w-1/5 lg:flex p-2 items-center justify-center">
@@ -399,6 +500,8 @@ const AudioPlayer = ({ className }: Props) => {
         </button>
       </div>
 >>>>>>> a715526 (audio player layout/ basic functionality)
+=======
+>>>>>>> 401ef41 (upload still loading data to ram)
     </div>
   );
 };
--- src/config/api.ts
+++ src/config/api.ts
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 export const baseUrl = "https://musicc-back.vercel.app/api";
 
 export const publicUrl = "https://musicc-back.vercel.app/";
-=======
-export const baseUrl = "http://192.168.38.202:8000/api";
-
-export const publicUrl = "http://192.168.38.202:8000/";
->>>>>>> 1fd1ad8 (audio player layout/ basic functionality)
